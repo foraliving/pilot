a3d8cebe92ad6fcc8f9d569af227c2919153b17d
Noelia Pazos <noelia.pazos@viaro.net>
2017-01-25 09:27:50 -0600

    Sprint 9 - Add teacher's approval functionality

---
+++ foraliving/static/js/teacher/group.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    $(".status").change(function () {
+        var video_id = $(this).val();
+        if ($(this).is(':checked')) {
+            var flag_id = 0;
+            $(this).siblings('label').html('Approved');
+            $(this).parent().removeClass('link').addClass('page-title');
+
+        }
+        else {
+            flag_id = 1;
+            $(this).siblings('label').html('Approve');
+            $(this).parent().removeClass('page-title').addClass('link');
+
+        }
+
+
+        var url = "/foraliving/video/update/" + video_id + "/" + flag_id + "/"
+        $.ajax({
+            method: "GET",
+            url: url,
+            async: true,
+            contentType: "application/json"
+        }).done(function (data) {
+        });
+    });
+});
