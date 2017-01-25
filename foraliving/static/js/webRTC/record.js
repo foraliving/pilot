e885864667e114137b179ad9921b39ad6ee751dc
Adolfo <adolfo.morales@viaro.net>
2017-01-25 12:39:35 -0600

    Add more space between 'select camera' label and the select

--- foraliving/static/js/webRTC/record.js
+++ foraliving/static/js/webRTC/record.js
@@ -339,7 +339,8 @@ function timer() {
 }
 
 function save() {
-
+    $(".bootstrap-select .show-tick").css('z-index', 0);
+    $("button.dropdown-toggle").css('z-index', 0);
     var sizeTheOverlays = function () {
         $(".overlay").resize().each(function () {
             var h = $(this).parent().outerHeight();
