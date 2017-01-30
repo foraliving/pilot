df448dcaf537a80f613a3cc03b29a1a0f42c11a0
viaro-adolfo <adolfo.morales@viaro.net>
2017-01-30 12:31:11 -0600

    Remove unused URL

--- foraliving_project/settings.py
+++ foraliving_project/settings.py
@@ -145,6 +145,3 @@ STATIC_URL = '/static/'
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 MEDIA_ROOT = PROJECT_PATH + '/media'
 MEDIA_URL = '/media/'
-
-
-STUDENTS_TEMPLATE = '/media/files/FAL_classroom_roster.csv'
